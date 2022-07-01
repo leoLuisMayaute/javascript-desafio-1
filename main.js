265f77b37c5c63fa52be4e1bc894bad83b564bf4
Leo Mayaute <leomayaute85@gmail.com>
2022-07-01 10:07:42 -0500

    actualizacion

---
+++ main.js
@@ -0,0 +1,7 @@
+let tablaDeMultiplicar = 2;
+let resultado;
+
+for (let i = 0; i <= 10; i++){
+    resultado = tablaDeMultiplicar * i;
+    console.log(`${tablaDeMultiplicar} x ${i} es igual a ${resultado}`);
+}
